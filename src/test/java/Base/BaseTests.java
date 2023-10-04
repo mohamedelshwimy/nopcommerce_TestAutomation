3a30a5fd0072c2d9e46aabe4bc2eb0a84d4e2253
mohamedelshwimy <mohamedahmedebrahim97@gmail.com>
2023-10-05 02:39:22 +0300

    nopcommerce first commit

---
+++ src/test/java/Base/BaseTests.java
@@ -0,0 +1,26 @@
+package Base;
+
+import Pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTests {
+    private WebDriver driver;
+    protected HomePage homePage;
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
+
+    @BeforeMethod
+    public void goToHomePage(){
+        driver.get("https://demo.nopcommerce.com/");
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
